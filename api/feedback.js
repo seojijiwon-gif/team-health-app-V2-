2d7f68b77c0077748c8cf88b920557cbd28efd0e
seojijiwon-gif <seojijiwon@gmail.com>
2026-03-22 00:24:07 +0900

    Update feedback.js

--- api/feedback.js
+++ api/feedback.js
@@ -1,6 +1,6 @@
 // api/feedback.js
 // Google Gemini 1.5 API Integration
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
